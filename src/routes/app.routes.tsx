d9906e605be52b889b5d029dacb43282917c7814
Lucas Cordeiro <lucas.cordeiro@mobilesaude.com.br>
2022-08-15 01:51:14 -0300

    configuração de rotas e instalação de icones

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,14 +3,49 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Home} from '../screens/Home';
 import {Tickets} from '../screens/Tickets';
 import {Account} from '../screens/Account';
-
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const {Navigator, Screen} = createBottomTabNavigator();
 export function AppRoutes() {
   return (
-    <Navigator>
-      <Screen name="Home" component={Home} />
-      <Screen name="Ingressos" component={Tickets} />
-      <Screen name="Conta" component={Account} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#0076CB',
+      }}>
+      <Screen
+        name="Eventos "
+        component={Home}
+        options={{
+          tabBarIcon: ({size, color}) => {
+            return <FontAwesome size={size} color={color} name="star" />;
+          },
+        }}
+      />
+      <Screen
+        name="Ingressos"
+        component={Tickets}
+        options={{
+          tabBarIcon: ({size, color}) => {
+            return <FontAwesome size={size} color={color} name="ticket" />;
+          },
+        }}
+      />
+      <Screen
+        name="Conta"
+        component={Account}
+        options={{
+          tabBarIcon: ({size, color}) => {
+            return (
+              <MaterialCommunityIcons
+                size={size}
+                color={color}
+                name="account"
+              />
+            );
+          },
+        }}
+      />
     </Navigator>
   );
 }
